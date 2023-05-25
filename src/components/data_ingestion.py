e15873280fbfaa555fc8649465d195b191964b51
SputnikTH <rachata9866@gmail.com>
2023-05-25 14:47:43 +0700

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,7 +2,7 @@ import os
 import sys
 from src.exception import CustomException
 from src.logger import logging
-import pandas as pd 
+import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
@@ -17,13 +17,13 @@ class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataIngestionConfig()
 
-    def intiate_data_ingestion(self):
-        logging.info("Enterwd the data ingestion method or component")
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('')
-            logging.info('Read the dataset as data frame')
+            df=pd.read_csv('notebook\data\supermarket_sales.csv')
+            logging.info('Read the dataset as dataframe')
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path))
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
@@ -34,7 +34,7 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Ingestion of the data is complete")
+            logging.info("Inmgestion of the data iss completed")
 
             return(
                 self.ingestion_config.train_data_path,
@@ -42,9 +42,11 @@ class DataIngestion:
 
             )
         except Exception as e:
-            raise CustomException(e, sys)
-
+            raise CustomException(e,sys)
+        
 if __name__=="__main__":
     obj=DataIngestion()
     obj.initiate_data_ingestion()
+
+    
 
